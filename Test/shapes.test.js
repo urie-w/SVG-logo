0f840db2f59679ffa818091eefc44748a8566dc5
urie-w <urieasia@gmail.com>
2024-08-28 18:03:07 -0600

    deleted and started over couldnt fix before, File index started

--- Test/shapes.test.js
+++ Test/shapes.test.js
@@ -1,23 +1,36 @@
-const { circle, triangle, square } = require('../lib/shapes');
+const { Circle, Triangle, Square } = require('./shapes');
 
-// A simply test to render a green circle
-test('circle render', () => {
-    const shape = new circle();
-    shape.setColor("green");
-    expect(shape.render()).toEqual('<circle cx="150" cy="100" r="80" fill="green" />');
+describe ('Shape', () =>{
+    it('should set color for the shape', () => { 
+        const shape = new Shape();
+        shape.setColor('red');
+        expect(shape.color).toEqual('red');
+    });
 });
 
-
-//A simple test for triangle thats red
-test('triangle render', () => {
-    const shape = new triangle();
-    shape.setColor("red");
-    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="red" />');
+describe('Square', () => {
+    it('should render a red square', () => {
+        const shape = new Square();
+        shape.setColor("red");
+        const expectedSvg = '<rect x="90" y="40" width="120" height="120" fill="red" />';
+        expect(shape.render()).toEqual('<rect x="90" y="40" width="120" height="120" fill="red" />');
+    });
 });
 
-// A simple test for a blue square
-test('square render', () => {
-    const shape = new square();
-    shape.setColor("blue");
-    expect(shape.render()).toEqual('<rect x="90" y="40" width="120" height="120" fill="blue" />');
+describe('Circle', () => {
+    it('should render a blue circle', () => {
+        const shape = new Circle();
+        shape.setColor("blue");
+        const expectedSvg = '<circle cx="150" cy="100" r="80" fill="blue" />';
+        expect(shape.render()).toEqual('<circle cx="150" cy="100" r="80" fill="blue" />');
+    });
+});
+
+describe('Triangle', () => {
+    it('should render a green triangle', () => {
+        const shape = new Triangle();
+        shape.setColor("green");
+        const expectedSvg = '<polygon points="150, 18 244, 182 56, 182" fill="green" />';
+        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="green" />');
+    });
 });
